86f643c9fd8c448aa21968d74bd4d170123b3338
pilosophos <48043580+pilosophos@users.noreply.github.com>
2023-06-20 22:04:52 -0700

    do not prevent coin sound if already playing

--- assets/js/insert-coin.js
+++ assets/js/insert-coin.js
@@ -4,7 +4,7 @@
   const coinSfx = new Audio("{{ '/assets/audio/coin.ogg' | relative_url }}");
   document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("insert-coin").addEventListener("click", () => {
-      coinSfx.play();
+      coinSfx.cloneNode(true).play();
       const credits = document.getElementById("credit-counter");
       credits.textContent = (parseInt(credits.textContent) + 1).toString().padStart(2, '0');
     });
