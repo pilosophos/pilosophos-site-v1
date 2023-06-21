e11b2524e1b95120204b399e87cb85d5d96da5b6
pilosophos <48043580+pilosophos@users.noreply.github.com>
2023-06-20 21:59:08 -0700

    load coin sound to begin

--- assets/js/insert-coin.js
+++ assets/js/insert-coin.js
@@ -1,9 +1,10 @@
 ---
 ---
 (function(){
+  const coinSfx = new Audio("{{ '/assets/audio/coin.ogg' | relative_url }}");
   document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("insert-coin").addEventListener("click", () => {
-      new Audio("{{ '/assets/audio/coin.ogg' | relative_url }}").play();
+      coinSfx.play();
       const credits = document.getElementById("credit-counter");
       credits.textContent = (parseInt(credits.textContent) + 1).toString().padStart(2, '0');
     });
