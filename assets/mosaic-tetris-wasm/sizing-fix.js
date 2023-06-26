e427a843d12f3550b1543dc49fffcb6617190cc2
pilosophos <48043580+pilosophos@users.noreply.github.com>
2023-06-26 00:06:02 -0700

    make it look right on mobile too

--- assets/mosaic-tetris-wasm/sizing-fix.js
+++ assets/mosaic-tetris-wasm/sizing-fix.js
@@ -1,3 +1,9 @@
+/**
+ * This crap fixes a weird display bug that happens with the controls definition list
+ * when the right-floating mosaic tetris demo's height extends into the same
+ * vertical area as the definition list.
+ * Forcing the definition list to be 100% minus the width of the demo fixes it.
+ */
 (function() {
   const demoWidth = document.getElementById("demo").offsetWidth;
   const controls = document.getElementById("controls");
